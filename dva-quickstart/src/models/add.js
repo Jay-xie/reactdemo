4b70ab333e0abc1c7c64e5e077eb653153dccee0
jiexie7 <XieJie7credit>
2018-08-07 19:05:08 +0800

    list

---
+++ dva-quickstart/src/models/add.js
@@ -0,0 +1,12 @@
+export default {
+  namespace: 'inputs',
+  state: {
+    input: 'name',
+  },
+  reducers: {
+    change(state, { payload: name }){
+      return  {input: name}
+    },
+  },
+};
+  
